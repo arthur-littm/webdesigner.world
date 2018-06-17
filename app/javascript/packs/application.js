da0564276da6f1dc3206a07dde561db815924231
Arthur Littmann <arthur.littmann@gmail.com>
2018-06-17 11:25:02 +0200

    added sweet-scroll

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,9 @@
 import "bootstrap";
+import SweetScroll from 'sweet-scroll';
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 
 initUpdateNavbarOnScroll();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const scroller = new SweetScroll({/* some options */});
+}, false);
