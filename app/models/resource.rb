dd76f25b85493f2d76155c278f9b220ccd769d56
Arthur Littmann <arthur.littmann@gmail.com>
2018-06-17 12:00:02 +0200

    fixed common search list

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -3,4 +3,15 @@ class Resource < ApplicationRecord
 
   belongs_to :category
   has_many :common_searches
+
+  def list_common_searches
+    if common_searches.count > 1
+      arr = common_searches[0..-2].map {|s| "<code>#{s.text}</code>"}
+      searches = arr.join(", ")
+      searches << ", <code>#{common_searches.last.text}</code>."
+      return searches
+    else
+      return "<code>#{common_searches.first.text}</code>."
+    end
+  end
 end
