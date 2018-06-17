574975d234c575ae6308942165408262f5ac2728
Arthur Littmann <arthur.littmann@gmail.com>
2018-06-17 12:01:58 +0200

    fixed common search list

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,13 +4,13 @@ class Category < ApplicationRecord
 
   def section_title
     case self.name
+    when "rules" then "Follow the design <strong>rules</strong> of thumb"
     when "inspiration" then "Step 1 - find <strong>inspiration</strong>!"
     when "colors" then "Step 2 - pick the right <strong>colors</strong>!"
-    when "icons" then "Step 3 - add some awesome <strong>icons</strong>!"
-    when "fonts" then "Step 4 - pick good <strong>fonts</strong>!"
+    when "fonts" then "Step 3 - pick good <strong>fonts</strong>!"
+    when "icons" then "Step 4 - add some awesome <strong>icons</strong>!"
     when "images" then "Step 5 - find beautiful <strong>images</strong>!"
     when "articles" then "Finally, stay up to date!"
-    when "rules" then "Follow the design <strong>rules</strong> of thumb"
     else
       self.name
     end
